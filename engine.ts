a080967140bb027466bfbfc22be908346b7d3089
Peter Fernandes <supersonicandtails@gmail.com>
2015-06-07 19:43:07 -0400

    Begin PuzzleRender implementation

--- engine.ts
+++ engine.ts
@@ -12,24 +12,24 @@ enum CellStates { Clear, Marked, Question }
 
 class Engine
 {
-	_options: EngineOptions
-	_board: Array<Array<number>>
-	_boardHeight: number
-	_boardWidth: number
+	private options: EngineOptions
+	private board: Array<Array<number>>
+	private boardHeight: number
+	private boardWidth: number
 	
 	constructor (options:EngineOptions)
 	{
-		this._options = options;
+		this.options = options;
 		//this._board = this._CreateBoardFromPuzzle(this._options.Puzzle);
-		this._boardHeight = this._options.Puzzle.length;
-		this._boardWidth = this._options.Puzzle[0].length;
+		this.boardHeight = this.options.Puzzle.length;
+		this.boardWidth = this.options.Puzzle[0].length;
 	}
 	
 	StartGame ()
 	{
 		var timer = new Timer({
 			StartSeconds: 64,
-			UpdateCallback: this._options.OnTimerUpdateCallback
+			UpdateCallback: this.options.OnTimerUpdateCallback
 		});
 		
 		timer.Start();
@@ -56,9 +56,9 @@ class Engine
 	
 	_IsWinState (puzzle, board)
 	{
-		for (var i = 0; i < this._boardHeight; i++)
+		for (var i = 0; i < this.boardHeight; i++)
 		{
-			for (var j = 0; j < this._boardWidth; j++)
+			for (var j = 0; j < this.boardWidth; j++)
 			{
 				if (puzzle[i][j] === 1 && board[i][j] !== CellStates.Marked)
 					return false;
@@ -66,5 +66,97 @@ class Engine
 		}
 		
 		return true;
+	}
+}
+
+class PuzzleRenderer
+{
+	static DrawBoard (puzzle: number[][], container:HTMLElement)
+	{
+		var columnNumbers: number[][] = PuzzleRenderer.GenerateColumnNumbers(puzzle);
+		var rowNumbers: number[][] = PuzzleRenderer.GenerateRowNumbers(puzzle);
+		
+		console.log(columnNumbers);
+		console.log(rowNumbers);
+	}
+	
+	private static GenerateColumnNumbers (puzzle: number[][]) : number[][]
+	{
+		var numbers = [];
+		
+		for (var col = 0; col < puzzle[0].length; col++)
+		{
+			var currentColumnNumbers = [];
+			var consecutiveSetCells = 0;
+			
+			for (var row = 0; row < puzzle.length; row++)
+			{
+				if (puzzle[row][col] === 1)
+				{
+					consecutiveSetCells += 1;
+				}
+				else if (consecutiveSetCells !== 0)
+				{
+					currentColumnNumbers.push(consecutiveSetCells);
+					consecutiveSetCells = 0;
+				}
+			}
+			
+			if (consecutiveSetCells !== 0)
+			{
+				currentColumnNumbers.push(consecutiveSetCells);
+			}
+			
+			if (currentColumnNumbers.length > 0)
+			{
+				numbers.push(currentColumnNumbers);			
+			}
+			else
+			{
+				numbers.push(0);
+			}
+		}
+		
+		return numbers;
+	}
+	
+	private static GenerateRowNumbers (puzzle: number[][]) : number[][]
+	{
+		var numbers = [];
+		
+		for (var col = 0; col < puzzle[0].length; col++)
+		{
+			var currentColumnNumbers = [];
+			var consecutiveSetCells = 0;
+			
+			for (var row = 0; row < puzzle.length; row++)
+			{
+				if (puzzle[row][col] === 1)
+				{
+					consecutiveSetCells += 1;
+				}
+				else if (consecutiveSetCells !== 0)
+				{
+					currentColumnNumbers.push(consecutiveSetCells);
+					consecutiveSetCells = 0;
+				}
+			}
+			
+			if (consecutiveSetCells !== 0)
+			{
+				currentColumnNumbers.push(consecutiveSetCells);
+			}
+			
+			if (currentColumnNumbers.length > 0)
+			{
+				numbers.push(currentColumnNumbers);			
+			}
+			else
+			{
+				numbers.push(0);
+			}
+		}
+		
+		return numbers;
 	}
 }
