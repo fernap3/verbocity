5bf7c1c7b1675f892e2d5ad4646056685d4db27f
Peter Fernandes <supersonicandtails@gmail.com>
2015-06-14 23:22:46 -0400

    Only dismiss share prompt if the overlay is clicked, not the entire document

--- sharePrompt.ts
+++ sharePrompt.ts
@@ -9,7 +9,7 @@ class SharePrompt
 	private options: SharePromptOptions;
 	private textInput: HTMLInputElement;
 	private overlay: HTMLElement;
-	private onDocumentClick: (MouseEvent) => void;
+	private onOverlayClick: (MouseEvent) => void;
 	
 	constructor (options: SharePromptOptions)
 	{
@@ -19,7 +19,7 @@ class SharePrompt
 		this.overlay = document.createElement("div");
 		this.overlay.className = "clearOverlay";
 		
-		this.onDocumentClick = (MouseEvent) => {
+		this.onOverlayClick = (MouseEvent) => {
 			this.Close();
 		}
 	}
@@ -34,13 +34,13 @@ class SharePrompt
 		// Add the click listener in a different "thread" so the click to open
 		// the share prompt doesn't also trigger this.
 		setTimeout(() => {
-			document.body.addEventListener("click", this.onDocumentClick );
+			this.overlay.addEventListener("click", this.onOverlayClick );
 		}, 0);
 	}
 	
 	Close ()
 	{
-		document.body.removeEventListener("click", this.onDocumentClick );
+		this.overlay.removeEventListener("click", this.onOverlayClick );
 		this.options.Container.style.display = "none";
 		document.body.removeChild(this.overlay);
 		this.options.OnCloseCallback();
