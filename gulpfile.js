ef73b095eed210060892b6cc033ef4fbe897d03b
Peter Fernandes <supersonicandtails@gmail.com>
2015-06-20 15:23:14 -0400

    Add "deploy" task, which minifies the output JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
 var gulp = require('gulp');
-//var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 var less = require("gulp-less");
 var watch = require('gulp-watch');
 var typescript = require('gulp-typescript');
 var concat = require('gulp-concat');
 
-
-gulp.task('build-debug', ['build-typescript', 'less']);
-
-gulp.task('build-typescript', function () {
-      
-   var sourceTsFiles = ["puzzle.ts", "puzzles.ts", "centerer.ts", "timer.ts", "puzzleRenderer.ts",
+var sourceTsFiles = ["puzzle.ts", "puzzles.ts", "centerer.ts", "timer.ts", "puzzleRenderer.ts",
    "previewRenderer.ts", "timerRenderer.ts", "inputHandler.ts", "mainMenu.ts", "puzzleChooser.ts",
    "sharePrompt.ts", "engine.ts", "game.ts"];
 
+gulp.task('build-debug', ['build-typescript', 'less']);
+gulp.task('deploy', ['deploybuild-typescript', 'less']);
+
+
+gulp.task('build-typescript', function ()
+{
    var tsResult = gulp.src(sourceTsFiles)
                     .pipe(concat('verbocity.ts'))
                     .pipe(typescript({
@@ -26,7 +27,22 @@ gulp.task('build-typescript', function () {
         return tsResult.js.pipe(gulp.dest("js"));
 });
 
-gulp.task('less', function () {
+gulp.task('deploybuild-typescript', function ()
+{
+   var tsResult = gulp.src(sourceTsFiles)
+                    .pipe(concat('verbocity.ts'))
+                    .pipe(typescript({
+                        target: 'ES5',
+                        declarationFiles: false,
+                        noExternalResolve: true
+                    }));
+
+        tsResult.dts.pipe(uglify()).pipe(gulp.dest("js"));
+        return tsResult.js.pipe(uglify({mangle: true})).pipe(gulp.dest("js"));
+});
+
+gulp.task('less', function ()
+{
    gulp.src('*.less')
       .pipe(less())
       .pipe(gulp.dest('css'));
